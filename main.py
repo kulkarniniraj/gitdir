f28e0c62662e9e35e08f11dec01b6d9abedb71ac
Niraj Kulkarni <kulkarniniraj14@gmail.com>
2024-02-17 14:06:46 +0530

    1. Passthrough filesystem reference implementation 2. Create mount folder outside git folder and mount there

---
+++ main.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python
+
+#    Copyright (C) 2001  Jeff Epler  <jepler@unpythonic.dhs.org>
+#    Copyright (C) 2006  Csaba Henk  <csaba.henk@creo.hu>
+#
+#    This program can be distributed under the terms of the GNU LGPL.
+#    See the file COPYING.
+#
+
+from __future__ import print_function
+
+import os, sys
+from errno import *
+from stat import *
+import fcntl
+from threading import Lock
+# pull in some spaghetti to make this stuff work without fuse-py being installed
+try:
+    import _find_fuse_parts
+except ImportError:
+    pass
+import fuse
+from fuse import Fuse
+from pathlib import Path
+
+if not hasattr(fuse, '__version__'):
+    raise RuntimeError("your fuse-py doesn't know of fuse.__version__, probably it's too old.")
+
+fuse.fuse_python_api = (0, 2)
+
+fuse.feature_assert('stateful_files', 'has_init')
+
+
+def flag2mode(flags):
+    md = {os.O_RDONLY: 'rb', os.O_WRONLY: 'wb', os.O_RDWR: 'wb+'}
+    m = md[flags & (os.O_RDONLY | os.O_WRONLY | os.O_RDWR)]
+
+    if flags | os.O_APPEND:
+        m = m.replace('w', 'a', 1)
+
+    return m
+
+
+class GitFS(Fuse):
+
+    def __init__(self, *args, **kw):
+        Fuse.__init__(self, *args, **kw)
+        self.root = '/tmp'
+
+    def getattr(self, path):
+        return os.lstat("." + path)
+
+    def readlink(self, path):
+        return os.readlink("." + path)
+
+    def readdir(self, path, offset):
+        for e in os.listdir("." + path):
+            yield fuse.Direntry(e)
+
+    def unlink(self, path):
+        os.unlink("." + path)
+
+    def rmdir(self, path):
+        os.rmdir("." + path)
+
+    def symlink(self, path, path1):
+        os.symlink(path, "." + path1)
+
+    def rename(self, path, path1):
+        os.rename("." + path, "." + path1)
+
+    def link(self, path, path1):
+        os.link("." + path, "." + path1)
+
+    def chmod(self, path, mode):
+        os.chmod("." + path, mode)
+
+    def chown(self, path, user, group):
+        os.chown("." + path, user, group)
+
+    def truncate(self, path, len):
+        f = open("." + path, "a")
+        f.truncate(len)
+        f.close()
+
+    def mknod(self, path, mode, dev):
+        os.mknod("." + path, mode, dev)
+
+    def mkdir(self, path, mode):
+        os.mkdir("." + path, mode)
+
+    def utime(self, path, times):
+        os.utime("." + path, times)
+
+    def access(self, path, mode):
+        if not os.access("." + path, mode):
+            return -EACCES
+
+    def statfs(self):
+        return os.statvfs(".")
+
+    def fsinit(self):
+        os.chdir(self.root)
+
+    class GFSFile(object):
+
+        def __init__(self, path, flags, *mode):
+            self.file = os.fdopen(os.open("." + path, flags, *mode),
+                                  flag2mode(flags))
+            self.fd = self.file.fileno()
+            if hasattr(os, 'pread'):
+                self.iolock = None
+            else:
+                self.iolock = Lock()
+
+        def read(self, length, offset):
+            if self.iolock:
+                self.iolock.acquire()
+                try:
+                    self.file.seek(offset)
+                    return self.file.read(length)
+                finally:
+                    self.iolock.release()
+            else:
+                return os.pread(self.fd, length, offset)
+
+        def write(self, buf, offset):
+            if self.iolock:
+                self.iolock.acquire()
+                try:
+                    self.file.seek(offset)
+                    self.file.write(buf)
+                    return len(buf)
+                finally:
+                    self.iolock.release()
+            else:
+                return os.pwrite(self.fd, buf, offset)
+
+        def release(self, flags):
+            self.file.close()
+
+        def _fflush(self):
+            if 'w' in self.file.mode or 'a' in self.file.mode:
+                self.file.flush()
+
+        def fsync(self, isfsyncfile):
+            self._fflush()
+            if isfsyncfile and hasattr(os, 'fdatasync'):
+                os.fdatasync(self.fd)
+            else:
+                os.fsync(self.fd)
+
+        def flush(self):
+            self._fflush()
+            # cf. xmp_flush() in fusexmp_fh.c
+            os.close(os.dup(self.fd))
+
+        def fgetattr(self):
+            return os.fstat(self.fd)
+
+        def ftruncate(self, len):
+            self.file.truncate(len)
+
+        def lock(self, cmd, owner, **kw):
+            op = { fcntl.F_UNLCK : fcntl.LOCK_UN,
+                   fcntl.F_RDLCK : fcntl.LOCK_SH,
+                   fcntl.F_WRLCK : fcntl.LOCK_EX }[kw['l_type']]
+            if cmd == fcntl.F_GETLK:
+                return -EOPNOTSUPP
+            elif cmd == fcntl.F_SETLK:
+                if op != fcntl.LOCK_UN:
+                    op |= fcntl.LOCK_NB
+            elif cmd == fcntl.F_SETLKW:
+                pass
+            else:
+                return -EINVAL
+
+            fcntl.lockf(self.fd, op, kw['l_start'], kw['l_len'])
+
+    def main(self, *a, **kw):
+
+        self.file_class = self.GFSFile
+
+        return Fuse.main(self, *a, **kw)
+
+def create_folder(path:Path):
+    '''
+    Create folder if needed
+    '''
+    path.mkdir(parents=True, exist_ok=True)
+
+def main():
+
+    usage = """
+Userspace nullfs-alike: mirror the filesystem tree from some point on.
+
+""" + Fuse.fusage
+    
+    print(sys.argv)
+    folder = Path(sys.argv[1])
+    parent = folder.parent
+    mount = parent / 'mount'
+
+    # # print(folder, parent, mount)
+    sys.argv[1] = str(mount)
+    print(sys.argv)
+    # create_folder(mount)
+    # print(f'mounting to: {mount}')
+
+    server = GitFS(version="%prog " + fuse.__version__,
+                 usage=usage,
+                 dash_s_do='setsingle')
+
+    server.parser.add_option(mountopt="root", metavar="PATH", default='/',
+                             help="mirror filesystem from under PATH [default: %default]")
+    server.parse(values=server, errex=1)
+
+    try:
+        if server.fuse_args.mount_expected():
+            os.chdir(server.root)
+    except OSError:
+        print("can't enter root of underlying filesystem", file=sys.stderr)
+        sys.exit(1)
+
+    server.main()
+
+
+if __name__ == '__main__':
+    main()
